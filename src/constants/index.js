28ebc2f5ce144ce17a9164a0504244eea3c213e5
Vitor Rios <vitor.neto@br.experian.com>
2020-05-16 12:38:28 -0300

    Adicionando constantes para status de direcao de onMove do card

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const STATUS_CARD = {
+  TODO: 'TODO',
+  DOING: 'DOING',
+  DONE: 'DONE',
+  NONE: 'NONE'
+}
+
+export const DIRECTION_MOVE = {
+  onNext: 'ON_NEXT',
+  onGoBack: 'ON_GO_BACK',
+}
